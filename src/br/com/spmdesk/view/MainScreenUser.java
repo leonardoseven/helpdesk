c1e4c92987c81af6feab01e95574eb3b8d4ba2ec
leonematias1 <44536402+leonematias1@users.noreply.github.com>
2019-10-20 19:49:09 -0300

    ##

---
+++ src/br/com/spmdesk/view/MainScreenUser.java
@@ -0,0 +1,43 @@
+package br.com.spmdesk.view;
+
+import br.com.spmdesk.interfaces.ChamarTela;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class MainScreenUser implements ChamarTela {
+	public MainScreenUser(Stage stage) {
+		chamarTela(stage);
+	}
+
+	@Override
+	public void chamarTela(Stage stage) {
+		// TODO Auto-generated method stub
+		// Area dos botões do usuário
+		Button btnChamado = new Button("Abrir um chamado");
+		Button btnRequisicao = new Button("Abrir uma requisição");
+
+		// Criação do painel e customização
+		GridPane gridPainel = new GridPane();
+		gridPainel.setMinSize(500, 250);
+		
+		// Espaçamento dos elementos
+		gridPainel.setVgap(25);
+		gridPainel.setHgap(25);
+		
+		// Botões
+		btnChamado.setMinSize(200, 200);
+		btnRequisicao.setMinSize(200, 200);
+		gridPainel.add(btnChamado, 1, 1);
+		gridPainel.add(btnRequisicao, 3, 1);
+		btnChamado.setStyle("-fx-font-size:20;");
+		btnRequisicao.setStyle("-fx-font-size:20;");
+		
+		// Criação de cena e inicialização da mesma
+		Scene scene = new Scene(gridPainel);
+		stage.setScene(scene);
+		stage.setTitle("Tela Inicial");
+		stage.show();
+	}
+}
